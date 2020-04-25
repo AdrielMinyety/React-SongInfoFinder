da8a419377dbc082a71cfe945474aa5b699bf993
unknown <AM3114779@AM3114779.c3edu.online>
2020-04-25 14:52:46 -0400

    New repository

---
+++ precache-manifest.39663b5b6d9462921bfb1b3f9c8ad8d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07261ac26892659f605d133d48742f51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b7155c86202735f62dd",
+    "url": "./static/css/main.956516e3.chunk.css"
+  },
+  {
+    "revision": "78390a8e056f9b1abd39",
+    "url": "./static/js/2.78e2f5b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.78e2f5b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7155c86202735f62dd",
+    "url": "./static/js/main.4f308b62.chunk.js"
+  },
+  {
+    "revision": "e34859c85453423fd313",
+    "url": "./static/js/runtime-main.ef2af777.js"
+  }
+]);
